07740988711204bf6d274937c1c4116da302464e
divinemantraitmember2 <divinemantraitmember2@gmail.com>
2025-08-13 14:03:37 +0530

    upadeted

--- components/category/ProductGrid.jsx
+++ components/category/ProductGrid.jsx
@@ -3,7 +3,7 @@ export default function ProductGrid({productsData,catSlug}) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-2">
       { productsData !=null && productsData && productsData.map((product) => (
-        <ProductCard key={product.id} product={product} slug={catSlug} />
+        <ProductCard key={product.productID} product={product} slug={catSlug} />
       ))}
     </div>
   );
